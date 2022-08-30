ca321f80608d9181b7a2dc8a5dbafa1f586a7735
Web3Learner <programmedhart@protonmail.com>
2022-08-30 10:07:56 -0400

    commit

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import FrontPage from './components/FrontPage/FrontPage';
+
+function App() {
+
+
+  
+  return (
+    <>
+      <FrontPage />
+    </>
+  );
+}
+
+export default App;
