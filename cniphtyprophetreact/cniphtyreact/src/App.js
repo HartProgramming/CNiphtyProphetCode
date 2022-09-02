311bf94cb8bd0240fd6d879a56624d73a9844fd4
Web3Learner <programmedhart@protonmail.com>
2022-09-01 21:37:38 -0400

    commit

--- cniphtyprophetreact/cniphtyreact/src/App.js
+++ cniphtyprophetreact/cniphtyreact/src/App.js
@@ -1,7 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
 import Home from './components/Home/Home';
-import 
+import Merchandise from './components/Merchandise/Merchandise';
+import Lend from './components/Lend/Lend';
+import Borrow from './components/Borrow/Borrow';
+import Donate from './components/Donate/Donate';
+import CNFT from './components/CNFT/CNFT';
+import Cardano from './components/Cardano/Cardano';
 import './App.css';
 
 function App() {
@@ -9,8 +14,13 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Layout />}>
-          <Route index element={<Home />}/>
-          <Route path='borrow'
+          <Route index element={<Home />} />
+          <Route path='borrow' element={<Borrow />} />
+          <Route path='lend' element={<Lend />} />
+          <Route path='cnft' element={<CNFT />} />
+          <Route path='cardano' element={<Cardano />} />
+          <Route path='merchandise' element={<Merchandise />} />
+          <Route path='donate' element={<Donate />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- cniphtyprophetreact/cniphtyreact/src/components/MainApplication/MainApplication.js
+++
@@ -1,9 +0,0 @@
-import NavBar from "../NavBar/NavBar"
-
-function MainApplication() {
-    <>
-        <NavBar />
-    </>
-}
-
-export default MainApplication
